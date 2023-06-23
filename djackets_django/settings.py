d2a442362019fdadd122427b89ba9bda82b65f02
PANxxJ <ps789875@gmail.com>
2023-06-23 23:02:23 +0530

    comit

--- djackets_django/settings.py
+++ djackets_django/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'corsheaders',
     'djoser',
+    'product',
 ]
 CORS_ALLOWED_ORIGINS =[
     'http://localhost:8080',
@@ -126,6 +127,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL='/media/'
+MEDIA_ROOT=BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- djackets_django/urls.py
+++ djackets_django/urls.py
@@ -1,4 +1,5 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
 
@@ -6,5 +7,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1',include('djoser.urls')),
     path('api/v1',include('djoser.urls.authtoken')),
-]
+] + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
